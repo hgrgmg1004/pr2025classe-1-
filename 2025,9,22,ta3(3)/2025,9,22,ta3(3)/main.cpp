d0ac3bed3502c31810b5e50ae6454158fedc448a
hgrgmg1004 <ij325328@gmail.com>
2025-09-23 20:23:07 +0900

    main

---
+++ 2025,9,22,ta3(3)/2025,9,22,ta3(3)/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("=== Char / String & Escapes ===\n");
+    printf("Char : %c, code : %d\n", 'A', 65);
+    printf("Message :\n");
+    printf("%s\n\t%s\n", "First line", "Indented second line");
+    printf("%s\n", "Third line");
+    printf("Quotes:%s\n", "\"double quotes\" and 'single quotes'");
+    printf("%s\n", "Backslash : \\");
+    printf("Average : [%8.2f]\n", 87.35f);
+
+    return 0;
+}
