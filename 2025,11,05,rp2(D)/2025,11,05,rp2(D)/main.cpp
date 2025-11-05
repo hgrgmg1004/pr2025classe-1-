d33dca74d2c8efb5a6e455b480529d6e3888a2b2
hgrgmg1004 <ij325328@gmail.com>
2025-11-05 15:24:05 +0900

    main

--- 2025,11,05,rp2(D)/2025,11,05,rp2(D)/main.cpp
+++ 2025,11,05,rp2(D)/2025,11,05,rp2(D)/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() 
 {
-    for (int d = 2; d <= 9; d++) 
+    for (int d = 2; d <= 4; d++) 
     {
         cout << d << "´Ü" << endl;
         for (int n = 1; n <= 9; n++) 
