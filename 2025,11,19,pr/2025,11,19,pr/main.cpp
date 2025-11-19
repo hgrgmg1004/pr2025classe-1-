9922ed3eea484298f12cf7a7d86a8ad248907bde
hgrgmg1004 <ij325328@gmail.com>
2025-11-19 09:49:06 +0900

    main

--- 2025,11,19,pr/2025,11,19,pr/main.cpp
+++ 2025,11,19,pr/2025,11,19,pr/main.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-void calc(double (*p1)[2], double (*p2)[2], double* m, double* b) 
+int main() 
 {
-    double x1 = (*p1)[0];
-    double y1 = (*p1)[1];
-    double x2 = (*p2)[0];
-    double y2 = (*p2)[1];
+    double x1 = 5.7;
+    double y1 = 10.5;
+    double x2 = -2.7;
+    double y2 = 6.5;
+    double m;
+    double b;
     if (x2 != x1) 
     {
-        *m = (y2 - y1) / (x2 - x1);
-        *b = y1 - (*m) * x1;
+        m = (y2 - y1) / (x2 - x1);
+        b = y1 - m * x1;
     }
     else 
     {
-        *m = 0.0;
-        *b = 0.0;
+        m = 0.0;
+        b = 0.0;
     }
-}
-int main() 
-{
-    double data[2][2];
-    data[0][0] = 5.7;
-    data[0][1] = 10.5;
-    data[1][0] = -2.7;
-    data[1][1] = 6.5;
-    double slope;
-    double bias;
-    calc(&data[0], &data[1], &slope, &bias);
     cout << "Input 1st point information:" << endl;
-    cout << fixed << setprecision(1) << data[0][0] << " " << data[0][1] << endl;
+    cout << fixed << setprecision(1) << x1 << " " << y1 << endl;
     cout << "Input 2nd point information:" << endl;
-    cout << fixed << setprecision(1) << data[1][0] << " " << data[1][1] << endl;
+    cout << fixed << setprecision(1) << x2 << " " << y2 << endl;
     cout << "Calculated Output, line equation:" << endl;
-    cout << "slope: " << fixed << setprecision(4) << slope << endl;
-    cout << "bias: " << fixed << setprecision(4) << bias << endl;
+    cout << "slope: " << fixed << setprecision(4) << m << endl;
+    cout << "bias: " << fixed << setprecision(4) << b << endl;
     return 0;
 }
