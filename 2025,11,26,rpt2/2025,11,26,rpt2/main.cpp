bfd0d46032efa927d0d7aa51136fa9515982af4a
hgrgmg1004 <ij325328@gmail.com>
2025-11-25 20:41:13 +0900

    main

--- 2025,11,26,rpt2/2025,11,26,rpt2/main.cpp
+++ 2025,11,26,rpt2/2025,11,26,rpt2/main.cpp
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#define NUM_DATA 100
-#define END_COND 999
+#define ARRAY_SIZE 100
+#define SUBSET_SIZE 10
+#define MIN_RANGE 1
+#define MAX_RANGE 100
+#define END_COND 1112
 void GenRandSeed()
 {
     srand((unsigned int)(time(NULL)));
@@ -11,57 +14,84 @@ void GenRandSeed()
 }
 unsigned int GenRandNum(unsigned int nRange)
 {
+    unsigned int nRes = 0;
     unsigned int nNum = rand();
-    unsigned int nRes = (unsigned int)(nNum % nRange);
+    nRes = ((unsigned int)(nNum) % (nRange));
     return nRes;
+}
+void InitArrayWithRandom(int arr[], int nSize, int nMin, int nMax)
+{
+    int nRange = nMax - nMin + 1;
+    for (int i = 0; i < nSize; i++)
+    {
+        arr[i] = nMin + GenRandNum(nRange);
+    }
+}
+void CalcStatistics(const int* pStartPtr, int nCount)
+{
+    double dSum = 0.0;
+    double dMean = 0.0;
+    double dVariance = 0.0;
+    double dStdDev = 0.0;
+    for (int i = 0; i < nCount; i++)
+    {
+        dSum += *(pStartPtr + i);
+    }
+    dMean = dSum / nCount;
+    double dSquaredDiffSum = 0.0;
+    for (int i = 0; i < nCount; i++)
+    {
+        double dDiff = *(pStartPtr + i) - dMean;
+        dSquaredDiffSum += dDiff * dDiff;
+    }
+    dVariance = dSquaredDiffSum / nCount;
+    dStdDev = sqrt(dVariance);
+    printf("\n=== 통계 결과 ===\n");
+    printf("요소 개수: %d\n", nCount);
+    printf("총합: %.0f\n", dSum);
+    printf("평균: %.4f\n", dMean);
+    printf("분산: %.4f\n", dVariance);
+    printf("표준편차: %.4f\n", dStdDev);
+    printf("=================\n");
 }
 int main(void)
 {
     GenRandSeed();
-    int nArr[NUM_DATA];
-    int i = 0;
-    for (i = 0; i < NUM_DATA; i++)
+    int nNumbers[ARRAY_SIZE];
+    int nStartIdx = 0;
+    InitArrayWithRandom(nNumbers, ARRAY_SIZE, MIN_RANGE, MAX_RANGE);
+    printf("생성된 100개의 정수 (일부):\n");
+    for (int i = 0; i < 10; i++)
     {
-        nArr[i] = GenRandNum(1000);
+        printf("%d ", nNumbers[i]);
     }
-    int nStart = 0;
-    int nEnd = 0;
+    printf("... [생략]\n");
     while (1)
     {
-        printf("Input start and end index (0~99)\n");
-        printf("To end, input: 999 999\n");
-        scanf_s("%d %d", &nStart, &nEnd);
-
-        if (nStart == END_COND || nEnd == END_COND)
+        printf("\n통계를 계산할 시작 인덱스를 입력하세요 (0 ~ %d, 종료: %d): ",
+            ARRAY_SIZE - SUBSET_SIZE, END_COND);
+        if (scanf_s("%d", &nStartIdx) != 1)
         {
-            printf("Program end...\n");
-            break;
-        }
-        if (nStart < 0 || nEnd >= NUM_DATA || nStart > nEnd)
-        {
-            printf("Check your input...\n\n");
+            while (getchar() != '\n');
             continue;
         }
-        double dSum = 0.0;
-        int nCnt = nEnd - nStart + 1;
-        for (i = nStart; i <= nEnd; i++)
+        if (nStartIdx == END_COND)
         {
-            dSum += nArr[i];
+            printf("프로그램을 종료합니다.\n");
+            break;
         }
-        double dMean = dSum / nCnt;
-        double dVar = 0.0;
-        for (i = nStart; i <= nEnd; i++)
+        if (nStartIdx < 0 || nStartIdx > ARRAY_SIZE - SUBSET_SIZE)
         {
-            double dTemp = nArr[i] - dMean;
-            dVar += dTemp * dTemp;
+            printf("오류: 인덱스 범위가 올바르지 않습니다. 다시 입력해주세요.\n");
+            continue;
         }
-        dVar = dVar / nCnt;
-        double dStd = sqrt(dVar);
-        printf("Sum = %.3lf\n", dSum);
-        printf("Mean = %.3lf\n", dMean);
-        printf("Variance = %.3lf\n", dVar);
-        printf("Std = %.3lf\n\n", dStd);
+        printf("선택된 10개의 정수 (인덱스 %d부터):\n", nStartIdx);
+        for (int i = 0; i < SUBSET_SIZE; i++)
+        {
+            printf("%d ", nNumbers[nStartIdx + i]);
+        }
+        printf("\n");
+        CalcStatistics(&nNumbers[nStartIdx], SUBSET_SIZE);
     }
-
     return 0;
 }
--- 2025,11,26,rpt3/2025,11,26,rpt3/main.cpp
+++ 2025,11,26,rpt3/2025,11,26,rpt3/main.cpp
@@ -7,9 +7,9 @@ int main(void)
     double dDot = 0.0;
     double C[VEC];
     int i = 0;
-    printf("Input vector A (3 numbers):\n");
+    printf("백터 A (3 numbers):\n");
     scanf_s("%lf %lf %lf", &A[0], &A[1], &A[2]);
-    printf("Input vector B (3 numbers):\n");
+    printf("백터 B (3 numbers):\n");
     scanf_s("%lf %lf %lf", &B[0], &B[1], &B[2]);
     for (i = 0; i < VEC; i++)
     {
@@ -18,7 +18,7 @@ int main(void)
     C[0] = A[1] * B[2] - A[2] * B[1];
     C[1] = A[2] * B[0] - A[0] * B[2];
     C[2] = A[0] * B[1] - A[1] * B[0];
-    printf("Dot = %.3lf\n", dDot);
-    printf("Cross = (%.3lf, %.3lf, %.3lf)\n", C[0], C[1], C[2]);
+    printf("내적 = %.3lf\n", dDot);
+    printf("외적 = (%.3lf, %.3lf, %.3lf)\n", C[0], C[1], C[2]);
     return 0;
 }
