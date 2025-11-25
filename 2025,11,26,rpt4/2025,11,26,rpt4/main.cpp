f69f1ad4535b02cd6c1b12b3453eb9014d25c199
hgrgmg1004 <ij325328@gmail.com>
2025-11-25 10:30:29 +0900

    main

--- 2025,11,26,rpt4/2025,11,26,rpt4/main.cpp
+++ 2025,11,26,rpt4/2025,11,26,rpt4/main.cpp
@@ -19,16 +19,18 @@ int main(void)
     GenRandSeed();
     int nCom[LOTTO];
     int nUser[LOTTO];
-    int i = 0, j = 0;
+    int i = 0;
+    int j = 0;
     int nSame = 0;
     for (i = 0; i < LOTTO; i++)
     {
         nCom[i] = GenRandNum(MAXNUM);
+
         for (j = 0; j < i; j++)
         {
             if (nCom[i] == nCom[j])
             {
-                i--; 
+                i--;
                 break;
             }
         }
@@ -36,7 +38,7 @@ int main(void)
     printf("Input your 6 lotto numbers (1~45):\n");
     for (i = 0; i < LOTTO; i++)
     {
-        scanf_s("%d", &nUser[i]);
+        (void)scanf_s("%d", &nUser[i]);
     }
     for (i = 0; i < LOTTO; i++)
     {
@@ -53,6 +55,18 @@ int main(void)
     {
         printf("%d ", nCom[i]);
     }
-    printf("\nMatched count = %d\n", nSame);
+    printf("\n");
+    if (nSame == 6)
+        printf("Result: 1st Prize (6 matches)\n");
+    else if (nSame == 5)
+        printf("Result: 2nd Prize (5 matches)\n");
+    else if (nSame == 4)
+        printf("Result: 3rd Prize (4 matches)\n");
+    else if (nSame == 3)
+        printf("Result: 4th Prize (3 matches)\n");
+    else if (nSame == 2)
+        printf("Result: 5th Prize (2 matches)\n");
+    else
+        printf("Result: No Prize (%d matches)\n", nSame);
     return 0;
 }
